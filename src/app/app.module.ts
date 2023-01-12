bdbb1f14d20e143ed60c7e7a4a87f486b69c7702
Aleksa Ignjatovic <aleksaignjatovic15@gmail.com>
2023-01-12 21:51:10 +0100

    app module updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,13 @@ import { HospitalModule } from "./modules/hospital/hospital.module";
 import { PagesModule } from "./modules/pages/pages.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './modules/toolbar/toolbar.component';
-import {ApplicationUserClient, DoctorClient, PatientHealthStateClient, ScheduleClient} from "./api/api-reference";
+import {
+  ApplicationUserClient,
+  AppointmentClient,
+  DoctorClient,
+  PatientHealthStateClient,
+  ScheduleClient
+} from "./api/api-reference";
 import {authInterceptorProviders} from "./helpers/auth.interceptor";
 import {NgToastModule} from "ng-angular-popup";
 import { NgxMaterialTimepickerModule } from "ngx-material-timepicker";
@@ -35,6 +41,7 @@ import {PatientHealthModule} from "./modules/patient-health/patient-health.modul
   providers: [ApplicationUserClient,
               DoctorClient,
               ScheduleClient,
+              AppointmentClient,
              authInterceptorProviders,PatientHealthStateClient],
   bootstrap: [AppComponent]
 })
